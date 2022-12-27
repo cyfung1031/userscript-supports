5653c128e65afd851e1b16eb64cad8b63eaadd39
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2022-12-27 20:18:10 +0900

    Update cookie-manager.user.js

--- cookie-manager.user.js
+++ cookie-manager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cookie Manager
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  For Developers Only. Control Cookies everywhere via DevTools
 // @author       CY Fung
 // @match        https://*/*
@@ -9,24 +9,24 @@
 // @icon         https://github.com/cyfung1031/userscript-supports/blob/main/icons/cookie-manager.png?raw=true
 // @grant        unsafeWindow
 // @license      MIT
-// @require      https://cdnjs.cloudflare.com/ajax/libs/js-cookie/3.0.1/js.cookie.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/js-cookie/3.0.1/js.cookie.min.js#sha512=wT7uPE7tOP6w4o28u1DN775jYjHQApdBnib5Pho4RB0Pgd9y7eSkAV1BTqQydupYDB9GBhTcQQzyNMPMV3cAew==
 // ==/UserScript==
-
+ 
 /* global Cookies */
-
+ 
 /*
 usage:
-
+ 
 cook.set('hello-world',100)
 console.log(cook.get('hello-world'))
 cook.remove('hello-world')
-
+ 
 cook.myvar = 'abc'
 console.log(cook.myvar)
 cook.myvar = null
-
+ 
 */
-
+ 
 (function () {
   'use strict';
   // Your code here...
