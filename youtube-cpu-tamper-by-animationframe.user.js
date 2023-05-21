68440e01dc86d55a98b4bf95e1e2a0e3a0f5e20a
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-05-21 10:42:55 +0900

    Update youtube-cpu-tamper-by-animationframe.user.js

--- youtube-cpu-tamper-by-animationframe.user.js
+++ youtube-cpu-tamper-by-animationframe.user.js
@@ -30,13 +30,84 @@ SOFTWARE.
 // @name:zh-TW          YouTube CPU Tamer by AnimationFrame
 // @name:zh-CN          YouTube CPU Tamer by AnimationFrame
 // @namespace           http://tampermonkey.net/
-// @version             2022.12.28.1
+// @version             2023.05.21
 // @license             MIT License
 // @description         Reduce Browser's Energy Impact for playing YouTube Video
 // @description:en      Reduce Browser's Energy Impact for playing YouTube Video
 // @description:ja      YouTubeビデオのエネルギーインパクトを減らす
 // @description:zh-TW   減少YouTube影片所致的能源消耗
 // @description:zh-CN   减少YouTube影片所致的能源消耗
+
+// @description:ko      YouTube 비디오 재생 시 브라우저의 에너지 영향을 줄입니다.
+// @description:ru      Снижает энергетическое воздействие браузера при воспроизведении видео на YouTube.
+// @description:af      Verminder die energie-impak van die blaaier vir YouTube-video speel
+// @description:az      YouTube videolarını oynamaq üçün Brauzer enerji təsirini azaldır
+// @description:id      Kurangi Dampak Energi Browser untuk memutar Video YouTube
+// @description:ms      Kurangkan Impak Tenaga Pelayar untuk memainkan Video YouTube
+// @description:bs      Smanji energetski uticaj preglednika za reprodukciju YouTube videa
+// @description:ca      Redueix l'impacte energètic del navegador per reproduir vídeos de YouTube
+// @description:cs      Snížit energetický dopad prohlížeče při přehrávání videí na YouTube
+// @description:da      Reducer browserens energipåvirkning for at afspille YouTube-video
+// @description:de      Reduzieren Sie die Energieauswirkungen des Browsers für die Wiedergabe von YouTube-Videos
+// @description:et      Vähendage YouTube'i video esitamiseks brauseri energiamõju
+// @description:es      Reduzca el impacto energético del navegador al reproducir videos de YouTube
+// @description:eu      Gutxitu nabigatzeko energiaren eragina YouTube bideoak erreproduzitzeko
+// @description:fr      Réduire l'impact énergétique du navigateur lors de la lecture de vidéos YouTube
+// @description:gl      Reduzca o impacto enerxético do navegador para reproducir vídeos de YouTube
+// @description:hr      Smanjite energetski utjecaj preglednika za reprodukciju YouTube videa
+// @description:zu      Qaqalitsha Umbono We-Energy we-Browser ukuze udlale i-Video ye-YouTube
+// @description:is      Minkaðu orkuáhrif vafra til að spila YouTube myndband
+// @description:it      Riduci l'impatto energetico del browser per la riproduzione di video di YouTube
+// @description:sw      Punguza Athari ya Nishati ya Kivinjari kwa kucheza Video za YouTube
+// @description:lv      Samaziniet pārlūkprogrammas enerģijas ietekmi YouTube video atskaņošanai
+// @description:lt      Sumažinkite naršyklės energijos poveikį žaidžiant „YouTube“ vaizdo įrašus
+// @description:hu      Csökkentse a böngésző energiaterhelését a YouTube videó lejátszásához
+// @description:nl      Verminder de energie-impact van de browser bij het afspelen van YouTube-video's
+// @description:uz      YouTube videoni tinglash uchun brauzer energiyasi ta'sirini kamaytirish
+// @description:pl      Zmniejsz zużycie energii przeglądarki podczas odtwarzania filmów na YouTube
+// @description:pt      Reduza o Impacto Energético do Navegador ao reproduzir Vídeos do YouTube
+// @description:pt-BR   Reduza o Impacto Energético do Navegador ao reproduzir Vídeos do YouTube
+// @description:ro      Reduceți impactul energetic al browser-ului pentru redarea videoclipurilor YouTube
+// @description:sq      Zvogëlo ndikimin e energjisë të shfletuesit për luajtjen e video YouTube
+// @description:sk      Znížte energetický dopad prehliadača pri prehrávaní videí na YouTube
+// @description:sl      Zmanjšajte energijski vpliv brskalnika pri predvajanju videoposnetkov YouTube
+// @description:sr      Smanjite energetski uticaj pregledača za reprodukciju YouTube videa
+// @description:fi      Vähennä selaimen energiankulutusta YouTube-videoiden toistossa
+// @description:sv      Minska webbläsarens energipåverkan för att spela YouTube-video
+// @description:vi      Giảm tác động năng lượng của trình duyệt khi phát Video YouTube
+// @description:tr      YouTube Videolarını Oynatırken Tarayıcının Enerji Etkisini Azaltın
+// @description:be      Змяншыце энергетычны ўплыў браўзара на прайграванне YouTube-відэа
+// @description:bg      Намалете енергийния влияние на браузъра при възпроизвеждане на видео в YouTube
+// @description:ky      YouTube видеонун ойнотуусунан башкаруу үчүн браузердеги энергиялык турмуштарды көмүштөштүрүү
+// @description:kk      YouTube-дың браузерде көрсету мүмкіндігін көмеге қысқартыңыз
+// @description:mk      Намалете ја енергетската присутност на пребарувачот за репродукција на YouTube видео
+// @description:mn      YouTube видеогийг тоглуулж буй хөтөчийн энерги хүчинг буурах
+// @description:uk      Зменште енергетичний вплив браузера на відтворення відео на YouTube
+// @description:el      Μειώστε την ενεργειακή επίδραση του προγράμματος περιήγησης για την αναπαραγωγή βίντεο στο YouTube
+// @description:hy      Փոքրանալիքայինը դանդարեցրեք բրաուզերի էներգիայի ազդեցությունը YouTube վիդեոների ներածման դեպքում
+// @description:ur      یوٹیوب ویڈیو کھیلنے کے لئے براؤزر کی توانائی پر اثر کم کریں
+// @description:ar      تقليل تأثير استهلاك الطاقة لمتصفح تشغيل مقاطع فيديو يوتيوب
+// @description:fa      کاهش تأثیر انرژی مرورگر برای پخش ویدئوی یوتیوب
+// @description:ne      युट्युब भिडियो खेल्नका लागि ब्राउजरको ऊर्जा प्रभाव कम गर्नुहोस्
+// @description:mr      YouTube व्हिडिओ चालवण्यासाठी ब्राउझरचे ऊर्जाचे प्रभाव कमी करा
+// @description:hi      यूट्यूब वीडियो चलाने के लिए ब्राउज़र की ऊर्जा प्रभाव को कम करें
+// @description:as      YouTube ভিডিঅ' প্ৰশ্ন কৰা ব্ৰাউজাৰৰ শক্তিৰ প্ৰভাৱ কমিয়া দিব
+// @description:bn      YouTube ভিডিও চালাতে ব্রাউজারের শক্তি প্রভাব কমান
+// @description:pa      YouTube ਵਿਡੀਓ ਚਲਾਉਣ ਲਈ ਬਰਾਉਜ਼ਰ ਦੀ ਊਰਜਾ ਪ੍ਰਭਾਵ ਘਟਾਓ
+// @description:gu      YouTube વિડિઓ ચલાવવા માટે બ્રાઉઝરનું ઊર્જા પ્રભાવ ઘટાડો
+// @description:or      YouTube ଭିଡିଓ ଚାଲାନ୍ତୁ ପାଇଁ ବ୍ରାଉଜରର ଶକ୍ତି ପ୍ରଭାବ କମାନ୍ତୁ
+// @description:ta      யூடியூப் வீடியோவை இயக்குவதற்கான உலாவியின் மிக்க விளைவுகளை குறைக்கவும்
+// @description:te      YouTube వీడియోను ప్రసారం చేయడానికి బ్రౌజర్ యొక్క శక్తి ప్రభావాన్ని తగ్గించుకోండి
+// @description:kn      YouTube ವೀಡಿಯೊಗಳನ್ನು ಪ್ರದರ್ಶಿಸಲು ಬ್ರೌಸರ್ ಯನ್ನು ಉಪಯೋಗಿಸುವಾಗ ಶಕ್ತಿ ಪ್ರಭಾವವನ್ನು ಕಡಿಮೆಗೊಳಿಸಿ
+// @description:ml      YouTube വീഡിയോ പ്രവർത്തിപ്പിക്കുവാൻ ബ്രൗസർയുടെ പ്രഭാവം കുറയ്ക്കുക
+// @description:si      YouTube වීඩියෝ චාරිකා කිරීම සඳහා බ්‍රවුසරයේ ඊම්ජි බලන්න
+// @description:th      ลดผลกระทบทางพลังงานของเบราว์เซอร์ในการเล่นวิดีโอ YouTube
+// @description:lo      ບຣາຣິໂຄດລາວເອີ້ນໃນການເພີ່ມເວັບວຽກຂອງ YouTube ສຳ ລັບການຂະໜາດໃນການເພີ່ມເວັບວຽກ
+// @description:my      YouTube ဗီဒီယိုများကို ဖွင့်ရန် Browser အတွက် Energy Impact ကိုအနိုင်ရန်
+// @description:ka      YouTube ვიდეოების დაკვრებისას ბრაუზერის ენერგიის შეცვლა
+// @description:am      YouTube ቪዲዮዎችን ለመቀነስ የባህሪውን አርእስት ግንኙነት ማድረግ
+// @description:km      បង្កើតការធ្វើបរិមាណលំអិតរបស់ការកំណត់ការដាក់នៅលើសម្ពាធរបស់ប្រព័ន្ធបញ្ចូលបន្ទាត់ YouTube
+
 // @author              CY Fung
 // @match               https://www.youtube.com/*
 // @match               https://www.youtube.com/embed/*
