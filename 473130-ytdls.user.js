13327fd67dc19ab253acbe3eafa88ff367c8dc37
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-08-16 18:12:41 +0900

    YtDLS | Improved Coding Mechanism
    * `localeLang` at the time of requesting (responding)
    * introduced `requestDeferred` to avoid fetching simultaneously (play safe)
    * ignore unnecessary / inappropriate subtitle response

--- 473130-ytdls.user.js
+++ 473130-ytdls.user.js
@@ -2,7 +2,7 @@
 // @name                YtDLS: YouTube Dual Language Subtitle (Modified)
 // @name:zh-CN          YtDLS: Youtube 双语字幕（改）
 // @name:zh-TW          YtDLS: Youtube 雙語字幕（改）
-// @version             2.0.3
+// @version             2.0.4
 // @description         Enhances YouTube with dual language subtitles.
 // @description:zh-CN   为YouTube添加双语字幕增强功能。
 // @description:zh-TW   增強YouTube的雙語字幕功能。
@@ -38,21 +38,9 @@ added m.youtube.com support based on two scripts (https://greasyfork.org/scripts
     const Promise = (async () => { })().constructor
     const _ah = ah
     const fetch = window.fetch.bind(window)
-    const pReady = new Promise(resolve => {
-        if (document.documentElement) return resolve()
-        let mo = new MutationObserver(function () {
-            if (mo && document.documentElement) {
-                mo.disconnect()
-                mo.takeRecords()
-                mo = null
-                resolve()
-            }
-        })
-        mo.observe(document, { childList: true, subtree: false })
-    })
+    const pReady = Promise.resolve()
     pReady.then(() => {
 
-        const localeLang = localeLangFn();
         let enableFullWidthSpaceSeparation = true
         function encodeFullwidthSpace(text) {
             if (!enableFullWidthSpaceSeparation) return text
@@ -62,23 +50,47 @@ added m.youtube.com support based on two scripts (https://greasyfork.org/scripts
             if (!enableFullWidthSpaceSeparation) return text
             return text.replace(/\n2\n/g, '\u3000').replace(/\n1\n/g, '\n')
         }
+        let requestDeferred = Promise.resolve()
         _ah.proxy({
             onRequest: (config, handler) => {
-                handler.next(config);
+                handler.next(config)
             },
             onResponse: (response, handler) => {
                 function defaultAction() {
                     handler.resolve(response)
                 }
+                const o = {}
                 try {
-                    if (!response.config.url.includes('/api/timedtext') || response.config.url.includes('&translate_h00ked')) return defaultAction()
+
+                    /** @type {string} */
+                    const originalReqUrl = response.config.url
+                    if (!originalReqUrl.includes('/api/timedtext') || originalReqUrl.includes('&translate_h00ked')) return defaultAction()
                     let defaultJson = null
                     if (response.response) {
                         const jsonResponse = JSON.parse(response.response)
                         if (jsonResponse.events) defaultJson = jsonResponse
                     }
                     if (defaultJson === null) return defaultAction()
-                    let lines = []
+
+                    const localeLang = localeLangFn()
+                    const langIdx = originalReqUrl.indexOf('lang=')
+                    if (langIdx > 5) {
+
+                        // &key=yt8&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3
+                        // &key=yt8&lang=ja&fmt=json3&xorb=2&xobt=3&xovt=3
+                        // &key=yt8&lang=ja&name=Romaji&fmt=json3&xorb=2&xobt=3
+
+                        let ulc = originalReqUrl.charAt(langIdx - 1)
+                        if (ulc === '?' || ulc === '&') {
+                            let usp = new URLSearchParams(originalReqUrl.substring(langIdx))
+                            let uspLang = usp.get('lang')
+                            let uspName = usp.get('name')
+                            if (uspName === 'Romaji') return defaultAction()
+                            if (typeof uspLang === 'string' && uspLang.toLocaleLowerCase() === localeLang.toLocaleLowerCase()) return defaultAction()
+                        }
+
+                    }
+                    const lines = []
                     for (const event of defaultJson.events) {
                         for (const seg of event.segs) {
                             if (seg && typeof seg.utf8 === 'string') {
@@ -86,48 +98,78 @@ added m.youtube.com support based on two scripts (https://greasyfork.org/scripts
                             }
                         }
                     }
+                    if (lines.length === 0) return defaultAction()
                     let linesText = lines.join('\n')
                     linesText = encodeFullwidthSpace(linesText)
-                    let q = encodeURIComponent(linesText)
-                    fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${localeLang}&dj=1&dt=t&dt=rm&q=${q}`)
-                        .then(res => {
-                            return res.json()
-                        })
-                        .then(result => {
-                            let resultText = result.sentences.map((function (s) {
-                                return "trans" in s ? s.trans : "";
-                            })).join("")
-                            resultText = decodeFullwidthSpace(resultText)
-                            return resultText.split("\n");
-                        })
-                        .then(translatedLines => {
-                            const addTranslation = (line, idx) => {
-                                if (line !== lines[i + idx]) return line
-                                let translated = translatedLines[i + idx]
-                                if (line === translated) return line
-                                return `${line}\n${translated}`
-                            }
-                            let i = 0
-                            for (const event of defaultJson.events) {
-                                for (const seg of event.segs) {
-                                    if ('utf8' in seg && typeof seg.utf8 === 'string') {
-                                        let s = seg.utf8.split('\n')
-                                        let st = s.map(addTranslation)
-                                        seg.utf8 = st.join('\n')
-                                        i += s.length
-                                    }
-                                }
-                            }
-                            response.response = JSON.stringify(defaultJson)
-                            handler.resolve(response)
-                        }).catch(e => {
-                            console.warn(e)
-                            defaultAction()
-                        })
+                    const q = encodeURIComponent(linesText)
+                    o.defaultJson = defaultJson
+                    o.lines = lines
+                    o.requestURL = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${localeLang}&dj=1&dt=t&dt=rm&q=${q}`
                 } catch (e) {
                     console.warn(e)
                     defaultAction()
                 }
+
+                function fetchData() {
+                    return new Promise(requestDeferredResolve => {
+
+                        fetch(o.requestURL, {
+                            method: "GET",
+                            headers: {
+                                "Accept": "application/json",
+                                "Accept-Encoding": "gzip, deflate, br"
+                            },
+                            credentials: "omit",
+                            referrerPolicy: "no-referrer",
+                            redirect: "error",
+                            keepalive: false,
+                            cache: "default"
+                        })
+                            .then(res => {
+                                requestDeferredResolve()
+                                return res.json()
+                            })
+                            .then(result => {
+                                let resultText = result.sentences.map((function (s) {
+                                    return "trans" in s ? s.trans : ""
+                                })).join("")
+                                resultText = decodeFullwidthSpace(resultText)
+                                return resultText.split("\n")
+                            })
+                            .then(translatedLines => {
+                                const { lines, defaultJson } = o
+                                o.lines = null
+                                o.defaultJson = null
+                                const addTranslation = (line, idx) => {
+                                    if (line !== lines[i + idx]) return line
+                                    let translated = translatedLines[i + idx]
+                                    if (line === translated) return line
+                                    return `${line}\n${translated}`
+                                }
+                                let i = 0
+                                for (const event of defaultJson.events) {
+                                    for (const seg of event.segs) {
+                                        if (seg && typeof seg.utf8 === 'string') {
+                                            let s = seg.utf8.split('\n')
+                                            let st = s.map(addTranslation)
+                                            seg.utf8 = st.join('\n')
+                                            i += s.length
+                                        }
+                                    }
+                                }
+                                response.response = JSON.stringify(defaultJson)
+                                handler.resolve(response)
+                            }).catch(e => {
+                                console.warn(e)
+                                defaultAction()
+                            })
+
+                    })
+
+                }
+
+                requestDeferred = requestDeferred.then(fetchData)
+
             }
         })
 
