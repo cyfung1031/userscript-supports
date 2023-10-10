720c999f669ad0c323c043aa56763d3f54f345e8
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-10-11 02:29:04 +0900

    Update 473972.user.js

--- 473972.user.js
+++ 473972.user.js
@@ -2,7 +2,7 @@
 // @name        YouTube JS Engine Tamer
 // @namespace   UserScripts
 // @match       https://www.youtube.com/*
-// @version     0.6.21
+// @version     0.6.22
 // @license     MIT
 // @author      CY Fung
 // @icon        https://github.com/cyfung1031/userscript-supports/raw/main/icons/yt-engine.png
@@ -40,9 +40,9 @@
   const FIX_Shady = true;
 
   const FIX_ytAction_ = true; // ytd-app
-  const FIX_onVideoDataChange = false;
+  const FIX_onVideoDataChange = true;
   // const FIX_onClick = true;
-  const FIX_onStateChange = true;
+  const FIX_onStateChange = false;
   const FIX_onLoopRangeChange = false;
   const FIX_maybeUpdateFlexibleMenu = true; // ytd-menu-renderer
   const FIX_VideoEVENTS_v2 = true; // true might cause bug in switching page
