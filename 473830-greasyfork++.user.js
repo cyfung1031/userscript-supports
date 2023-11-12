d146320deef5caad238c7d9b35519b345a596559
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-11-13 07:51:53 +0900

    Greasy Fork++ | Script Link (CDN)

--- 473830-greasyfork++.user.js
+++ 473830-greasyfork++.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name               Greasy Fork++
 // @namespace          https://github.com/iFelix18
-// @version            3.2.28
+// @version            3.2.29
 // @author             CY Fung <https://greasyfork.org/users/371179> & Davide <iFelix18@protonmail.com>
 // @icon               https://www.google.com/s2/favicons?domain=https://greasyfork.org
 // @description        Adds various features and improves the Greasy Fork experience
@@ -1626,17 +1626,18 @@ const mWindow = (() => {
         if (element.nodeName === 'LI' && element.hasAttribute('data-script-id') && element.getAttribute('data-script-id') === `${scriptID}` && element.getAttribute('data-script-language') === 'js') {
 
             const version = element.getAttribute('data-script-version') || ''
-            const name = element.getAttribute('data-script-name') || ''
+            // const name = element.getAttribute('data-script-name') || ''
             // if (!/[^\x00-\x7F]/.test(name)) {
 
-            const scriptName = useHashedScriptName ? qexString(await digestMessage(`${+scriptID} ${version}`, 'SHA-1')).substring(0, 8) : encodeURI(name);
-            const token = useHashedScriptName ? `${scriptName.substring(0, 2)}${scriptName.substring(scriptName.length - 2, scriptName.length)}` : String.fromCharCode(Date.now() % 26 + 97) + Math.floor(Math.random() * 19861 + 19861).toString(36);
+            // const scriptName = useHashedScriptName ? qexString(await digestMessage(`${+scriptID} ${version}`, 'SHA-1')).substring(0, 8) : encodeURI(name);
+            // const token = useHashedScriptName ? `${scriptName.substring(0, 2)}${scriptName.substring(scriptName.length - 2, scriptName.length)}` : String.fromCharCode(Date.now() % 26 + 97) + Math.floor(Math.random() * 19861 + 19861).toString(36);
             // let scriptFilename = `${encodeURI(name)}.user.js`;
-            const scriptFilename = `${scriptName}.user.js`;
+            // const scriptFilename = `${scriptName}.user.js`;
             _baseScript = {
                 id: +scriptID,
                 // name: name,
-                code_url: `https://${location.hostname}/scripts/${scriptID}-${token}/code/${scriptFilename}`,
+                // code_url: `https://${location.hostname}/scripts/${scriptID}-${token}/code/${scriptFilename}`,
+                code_url: `https://update.${location.hostname}/scripts/${scriptID}.user.js`,
                 version: version
             }
             // }
