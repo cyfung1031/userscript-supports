cfaffb398a35b263451b1fb6c0db023f0d14ad33
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-12-09 17:18:29 +0900

    YouTube EXPERIMENT_FLAGS Tamer | Fix

--- 470428.user.js
+++ 470428.user.js
@@ -2,7 +2,7 @@
 // @name        YouTube EXPERIMENT_FLAGS Tamer
 // @namespace   UserScripts
 // @match       https://www.youtube.com/*
-// @version     1.3.18
+// @version     1.3.19
 // @license     MIT
 // @author      CY Fung
 // @icon        https://github.com/cyfung1031/userscript-supports/raw/main/icons/yt-engine.png
@@ -607,6 +607,7 @@
           const kl3 = kl % 3;
           const kl2 = kl % 2;
 
+          if (SET_POLYMER_FLAGS && key === 'polymer_enable_mdx_queue') continue;
 
           if (FLAG_STRATEGY_01) {
 
@@ -980,7 +981,7 @@
       if (SET_POLYMER_FLAGS) {
         EXPERIMENT_FLAGS.polymer_video_renderer_defer_menu = true;
         EXPERIMENT_FLAGS.polymer_on_demand_shady_dom = true;
-        EXPERIMENT_FLAGS.polymer_enable_mdx_queue = true;
+        // EXPERIMENT_FLAGS.polymer_enable_mdx_queue = true;
       }
 
 
