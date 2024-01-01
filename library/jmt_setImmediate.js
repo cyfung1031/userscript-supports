6061b05928b7e5cfdc3a3d0055ae047b7adc2b29
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-01-01 16:13:31 +0900

    Update jmt_setImmediate.js

--- library/jmt_setImmediate.js
+++ library/jmt_setImmediate.js
@@ -1,8 +1,9 @@
 // ======================================= setImmediate =======================================
 // 
 // GitHub: https://github.com/YuzuJS/setImmediate
-// production file: https://cdnjs.cloudflare.com/ajax/libs/setImmediate/1.0.5/setImmediate.js
-// minified production file: https://cdnjs.cloudflare.com/ajax/libs/setImmediate/1.0.5/setImmediate.min.js
+// based on version 1.0.5 -  https://cdnjs.cloudflare.com/ajax/libs/setImmediate/1.0.5/setImmediate.js
+// modified by CY Fung => version 1.1.0
+// ES2015+ without supporting IE; adjusted `currentlyRunningATask` behavior
 /**
  * 
  * 
@@ -30,46 +31,50 @@
  * 
  */
 
-(function (_self_, undefined) {
+(function (global, undefined) {
     "use strict";
 
-    const global = (_self_.jmt || (_self_.jmt = {}));
+    var attachTo = (global.jmt || (global.jmt = {}));
 
-    if (global.setImmediate) {
+    if (global.setImmediate) { // Node, Deno, Bun, etc
         return;
     }
 
     var nextHandle = 1; // Spec says greater than zero
-    var tasksByHandle = {};
+    /**
+    * @typedef {Object} Task
+    * @property {Function} callback - Callback Function
+    * @property {any[] | number} args - Callback Arguments
+    */
+    var /** @type { Map<number, Task> } */ tasksByHandle = new Map();
     var currentlyRunningATask = false;
+    var currentlyRunningATaskEnable = typeof AbortSignal !== 'function';
+    // Ignoring currentlyRunningATask in Chrome 66+, Edge 16+, Firefox 57+, Opera 53+, Safari 11.1+
     var doc = global.document;
-    var registerImmediate;
+    var /** @type { (handle: number) => void 0 } */ registerImmediate;
 
-    function setImmediate(callback) {
+    /** @param { Function | string } callback */
+    function setImmediate(callback, ...args) {
         // Callback can either be a function or a string
         if (typeof callback !== "function") {
-            callback = new Function("" + callback);
-        }
-        // Copy function arguments
-        var args = new Array(arguments.length - 1);
-        for (var i = 0; i < args.length; i++) {
-            args[i] = arguments[i + 1];
+            callback = new Function(`${callback}`);
         }
         // Store and register the task
-        var task = { callback: callback, args: args };
-        tasksByHandle[nextHandle] = task;
+        var task = { callback: callback, args: args.length ? args : 0 };
+        tasksByHandle.set(nextHandle, task);
         registerImmediate(nextHandle);
         return nextHandle++;
     }
 
     function clearImmediate(handle) {
-        delete tasksByHandle[handle];
+        tasksByHandle.delete(handle);
     }
 
     function run(task) {
         var callback = task.callback;
         var args = task.args;
         switch (args.length) {
+            case undefined:
             case 0:
                 callback();
                 break;
@@ -91,12 +96,12 @@
     function runIfPresent(handle) {
         // From the spec: "Wait until any invocations of this algorithm started before this one have completed."
         // So if we're currently running a task, we'll need to delay this invocation.
-        if (currentlyRunningATask) {
+        if (currentlyRunningATaskEnable && currentlyRunningATask) {
             // Delay by doing a setTimeout. setImmediate was tried instead, but in Firefox 7 it generated a
             // "too much recursion" error.
             setTimeout(runIfPresent, 0, handle);
         } else {
-            var task = tasksByHandle[handle];
+            var task = tasksByHandle.get(handle);
             if (task) {
                 currentlyRunningATask = true;
                 try {
@@ -120,12 +125,12 @@
         // where `global.postMessage` means something completely different and can't be used for this purpose.
         if (global.postMessage && !global.importScripts) {
             var postMessageIsAsynchronous = true;
-            var oldOnMessage = global.onmessage;
-            global.onmessage = function () {
+            var mfn = function () {
                 postMessageIsAsynchronous = false;
-            };
+            }
+            global.addEventListener('message', mfn, false);
             global.postMessage("", "*");
-            global.onmessage = oldOnMessage;
+            global.removeEventListener('message', mfn, false);
             return postMessageIsAsynchronous;
         }
     }
@@ -135,23 +140,20 @@
         // * https://developer.mozilla.org/en/DOM/window.postMessage
         // * http://www.whatwg.org/specs/web-apps/current-work/multipage/comms.html#crossDocumentMessages
 
-        var messagePrefix = "setImmediate$" + Math.random() + "$";
+        var messagePrefix = `setImmediate$${Math.random()}$`;
         var onGlobalMessage = function (event) {
-            if (event.source === global &&
-                typeof event.data === "string" &&
-                event.data.indexOf(messagePrefix) === 0) {
-                runIfPresent(+event.data.slice(messagePrefix.length));
+            if (event.source === global) {
+                var data = event.data;
+                if (typeof data === "string" && data.startsWith(messagePrefix)) {
+                    runIfPresent(+data.slice(messagePrefix.length));
+                }
             }
         };
 
-        if (global.addEventListener) {
-            global.addEventListener("message", onGlobalMessage, false);
-        } else {
-            global.attachEvent("onmessage", onGlobalMessage);
-        }
+        global.addEventListener("message", onGlobalMessage, false);
 
         registerImmediate = function (handle) {
-            global.postMessage(messagePrefix + handle, "*");
+            global.postMessage(`${messagePrefix}${handle}`, "*");
         };
     }
 
@@ -167,48 +169,24 @@
         };
     }
 
-    function installReadyStateChangeImplementation() {
-        var html = doc.documentElement;
-        registerImmediate = function (handle) {
-            // Create a <script> element; its readystatechange event will be fired asynchronously once it is inserted
-            // into the document. Do so, thus queuing up the task. Remember to clean up once it's been called.
-            var script = doc.createElement("script");
-            script.onreadystatechange = function () {
-                runIfPresent(handle);
-                script.onreadystatechange = null;
-                html.removeChild(script);
-                script = null;
-            };
-            html.appendChild(script);
-        };
-    }
-
     function installSetTimeoutImplementation() {
         registerImmediate = function (handle) {
             setTimeout(runIfPresent, 0, handle);
         };
     }
 
-    // If supported, we should attach to the prototype of global, since that is where setTimeout et al. live.
-    var attachTo = Object.getPrototypeOf && Object.getPrototypeOf(global);
-    attachTo = attachTo && attachTo.setTimeout ? attachTo : global;
-
     // Don't get fooled by e.g. browserify environments.
     if ({}.toString.call(global.process) === "[object process]") {
         // For Node.js before 0.9
         installNextTickImplementation();
 
-    } else if (canUsePostMessage()) {
-        // For non-IE10 modern browsers
+    } else if (canUsePostMessage() && global.addEventListener) {
+        // For modern browsers
         installPostMessageImplementation();
 
     } else if (global.MessageChannel) {
         // For web workers, where supported
         installMessageChannelImplementation();
-
-    } else if (doc && "onreadystatechange" in doc.createElement("script")) {
-        // For IE 6–8
-        installReadyStateChangeImplementation();
 
     } else {
         // For older browsers
