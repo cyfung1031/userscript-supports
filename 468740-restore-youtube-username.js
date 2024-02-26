1732a6da08abdb2accc96e2408dd39d0759f10b0
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-02-26 22:34:39 +0900

    Restore YouTube Username from Handle | Message Fix

--- 468740-restore-youtube-username.js
+++ 468740-restore-youtube-username.js
@@ -26,7 +26,7 @@ SOFTWARE.
 // ==UserScript==
 // @name                Restore YouTube Username from Handle to Custom
 // @namespace           http://tampermonkey.net/
-// @version             0.11.005
+// @version             0.11.006
 // @license             MIT License
 
 // @author              CY Fung
@@ -2285,6 +2285,9 @@ SOFTWARE.
             const data = ((cnt || 0).data || 0);
             if (data) {
                 if (d = data.authorEndpoint) return d.browseEndpoint || null;
+                if (cnt.is === 'ytd-video-description-infocards-section-renderer') {
+                    return null; // skip console logging
+                }
                 console.log('no browseEndpoint can be found');
             }
         } else {
@@ -2293,6 +2296,9 @@ SOFTWARE.
                 if (d = __data.authorTextCommand) return d.browseEndpoint || null; // ytd-comment-renderer
                 if (d = __data.authorEndpoint) return d.browseEndpoint || null; // ytd-comment-view-model
                 if (d = (__data.data || 0).authorEndpoint) return d.browseEndpoint || null; // ytd-author-comment-badge-renderer
+                if (cnt.is === 'ytd-video-description-infocards-section-renderer') {
+                    return null; // skip console logging
+                }
                 console.log('no browseEndpoint can be found');
             }
         }
