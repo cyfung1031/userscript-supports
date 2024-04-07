a5e2083affaa13229bee73ffc196d36d4a160a31
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-04-07 14:14:39 +0900

    YouTube Super Fast Chat | CSS Fix

--- 469878-youtube-chat.js
+++ 469878-youtube-chat.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                YouTube Super Fast Chat
-// @version             0.61.8
+// @version             0.61.9
 // @license             MIT
 // @name:ja             YouTube スーパーファーストチャット
 // @name:zh-TW          YouTube 超快聊天
@@ -187,6 +187,7 @@
   const NO_FILTER_DROPDOWN_BORDER = true; // added in 2024.03.02
 
   const FIX_ANIMATION_TICKER_TEXT_POSITION = true; // CSS fix; experimential; added in 2024.04.07
+  const FIX_AUTHOR_CHIP_BADGE_POSITION = true;
 
   // ========= EXPLANTION FOR 0.2% @ step timing [min. 0.2%] ===========
   /*
@@ -685,6 +686,24 @@
 
   ` : '';
 
+  const cssText16_FIX_AUTHOR_CHIP_BADGE_POSITION = FIX_AUTHOR_CHIP_BADGE_POSITION ? `
+    #card  #author-name-chip > yt-live-chat-author-chip[single-line] {
+        flex-wrap: nowrap;
+        white-space: nowrap;
+        display: inline-flex;
+        flex-direction: row;
+        text-wrap: nowrap;
+        flex-shrink: 0;
+        align-items: center;    
+    }
+    
+    #card  #author-name-chip {
+        display: inline-flex;
+        flex-direction: row;
+        align-items: flex-start;
+    }
+  `:'';
+
 
   const addCss = () => `
 
@@ -933,6 +952,8 @@
     ${cssText14_NO_FILTER_DROPDOWN_BORDER}
 
     ${cssText15_FIX_ANIMATION_TICKER_TEXT_POSITION}
+
+    ${cssText16_FIX_AUTHOR_CHIP_BADGE_POSITION}
 
   `;
 
