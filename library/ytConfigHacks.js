4f4c25f7872d4dbe4ec01c0ed0047d071a4b5e8d
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-04-13 13:40:44 +0900

    Update ytConfigHacks.js

--- library/ytConfigHacks.js
+++ library/ytConfigHacks.js
@@ -2,7 +2,7 @@
 // @name         ytConfigHacks
 // @description  To provide a way to hack the yt.config_ such as EXPERIMENT_FLAGS
 // @author       CY Fung
-// @version      0.4.1
+// @version      0.4.2
 // @supportURL   https://github.com/cyfung1031/userscript-supports/
 // @license      MIT
 // @match        https://www.youtube.com/*
@@ -92,8 +92,8 @@ SOFTWARE.
         win.ytcsi = new Proxy(ytcsi, {
           get(target, prop, receiver) {
             if (prop === 'originalYtcsi') return target;
-            if (--restoreOriginalYtCSICount <= 0) restoreOriginalYtCSI && restoreOriginalYtCSI();
             detectConfigDone();
+            if (isValidConfig && --restoreOriginalYtCSICount <= 0) restoreOriginalYtCSI && restoreOriginalYtCSI();
             return target[prop];
           }
         });
