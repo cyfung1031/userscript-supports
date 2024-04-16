d513ef566d1e611738f44ceb012ded73f72c9799
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-04-16 20:55:44 +0900

    Create yt-flag-kevlar_watch_grid-false.user.js

---
+++ yt-flag-kevlar_watch_grid-false.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        YouTube: kevlar_watch_grid = false
+// @namespace   UserScripts
+// @match       https://www.youtube.com/*
+// @grant       none
+// @version     0.1.0
+// @author      CY Fung
+// @description Disable kevlar_watch_grid
+// @run-at      document-start
+// @inject-into page
+// @unwrap
+// @require     https://update.greasyfork.org/scripts/475632/1359675/ytConfigHacks.js
+// @license     MIT
+// ==/UserScript==
+
+(() => {
+  window._ytConfigHacks.add((config_) => {
+    const EXPERIMENT_FLAGS = config_.EXPERIMENT_FLAGS;
+    if (EXPERIMENT_FLAGS) {
+      EXPERIMENT_FLAGS.kevlar_watch_grid = false;
+    }
+  });
+})();
