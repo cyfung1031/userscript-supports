bdd6860958d4dbd8c7b8d0d4949fdb97e5281209
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-05-03 02:16:03 +0900

    Update 474214-fix-brave-yt-live-3.0.user.js

--- 474214-fix-brave-yt-live-3.0.user.js
+++ 474214-fix-brave-yt-live-3.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix Brave Bug for YouTube Live Chat
 // @namespace    UserScripts
-// @version      3.2
+// @version      3.3
 // @description  To Fix Brave Bug for YouTube Live Chat
 // @author       CY Fung
 // @license      MIT
@@ -20,6 +20,30 @@
 
     const insp = o => o ? (o.polymerController || o.inst || o || 0) : (o || 0);
 
+    const _ytIframeReloadDelay_ = window._ytIframeReloadDelay_ = window._ytIframeReloadDelay_ || (function () {
+        let pIfr = 0;
+        let url1 = null;
+        let url2 = null;
+        const pfn = resolve => {
+            if (!pIfr) {
+                pIfr = document.getElementById('d8y9c');
+                if (!pIfr) {
+                    pIfr = document.createElement('iframe');
+                    pIfr.id = 'd8y9c';
+                    pIfr.style.display = 'none';
+                    document.body.appendChild(pIfr);
+                }
+            }
+            pIfr.onload = resolve;
+            if (!url1) url1 = URL.createObjectURL(new Blob([], { type: 'text/html' }));
+            const c = url1;
+            url1 = url2;
+            url2 = c;
+            pIfr.contentDocument.location.replace(c);
+        };
+        return () => (new Promise(pfn)).catch(console.warn).then(() => { pIfr.onload = null; });
+    })();
+
     (async () => {
         'use strict';
 
@@ -33,30 +57,6 @@
         const cProto = cnt.constructor.prototype || 0;
 
         if(typeof cProto.urlChanged !== 'function' || cProto.urlChanged66) return;
-
-        const _ytIframeReloadDelay_ = window._ytIframeReloadDelay_ = window._ytIframeReloadDelay_ || (function () {
-            let pIfr = 0;
-            let url1 = null;
-            let url2 = null;
-            const pfn = resolve => {
-                if (!pIfr) {
-                    pIfr = document.getElementById('d8y9c');
-                    if (!pIfr) {
-                        pIfr = document.createElement('iframe');
-                        pIfr.id = 'd8y9c';
-                        pIfr.style.display = 'none';
-                        document.body.appendChild(pIfr);
-                    }
-                }
-                pIfr.onload = resolve;
-                if (!url1) url1 = URL.createObjectURL(new Blob([], { type: 'text/html' }));
-                const c = url1;
-                url1 = url2;
-                url2 = c;
-                pIfr.contentDocument.location.replace(c);
-            };
-            return () => (new Promise(pfn)).catch(console.warn).then(() => { pIfr.onload = null; });
-        })();
  
         cProto.urlChanged66 = cProto.urlChanged;
         cProto.urlChanged = function () {
