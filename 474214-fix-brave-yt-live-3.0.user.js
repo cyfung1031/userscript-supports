60e1a211191266053d1d8e794f1fcf5d55f5d2f2
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-05-26 22:45:33 +0900

    Fix Brave Bug for YouTube Live Chat | Implement Change

--- 474214-fix-brave-yt-live-3.0.user.js
+++ 474214-fix-brave-yt-live-3.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix Brave Bug for YouTube Live Chat
 // @namespace    UserScripts
-// @version      3.20
+// @version      3.21
 // @description  To Fix Brave Bug for YouTube Live Chat
 // @author       CY Fung
 // @license      MIT
@@ -20,6 +20,48 @@
 
     const insp = o => o ? (o.polymerController || o.inst || o || 0) : (o || 0);
 
+    const { _setAttribute, _insertBefore } = (() => {
+        let _setAttribute = Element.prototype.setAttribute;
+        try {
+            _setAttribute = ShadyDOM.nativeMethods.setAttribute;
+        } catch (e) { }
+        let _insertBefore = Node.prototype.insertBefore;
+        try {
+            _insertBefore = ShadyDOM.nativeMethods.insertBefore;
+        } catch (e) { }
+        return { _setAttribute, _insertBefore };
+    })();
+
+    const getDMHelper = () => {
+        let _dm = document.getElementById('d-m');
+        if (!_dm) {
+            _dm = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+            _dm.id = 'd-m';
+            _insertBefore.call(document.documentElement, _dm, document.documentElement.firstChild);
+        }
+        const dm = _dm;
+        dm._setAttribute = _setAttribute;
+        let j = 0;
+        let attributeName_;
+        while (dm.hasAttribute(attributeName_ = `dm-${Math.floor(Math.random() * 314159265359 + 314159265359).toString(36)}`)) {
+            // none
+        }
+        const attributeName = attributeName_;
+        let qr = null;
+        const mo = new MutationObserver(() => {
+            if (qr !== null) {
+                if (j > 8) j = 0;
+                qr = (qr(), null);
+            }
+        });
+        mo.observe(document, { childList: true, subtree: true, attributes: true });
+        return (resolve) => {
+            if (!qr) dm._setAttribute(attributeName, ++j);
+            return qr = resolve;
+            // return qr = afInterupter = resolve;
+        };
+    };
+
     (async () => {
         'use strict';
 
@@ -29,21 +71,28 @@
 
         if (!chat || chat.is !== 'ytd-live-chat-frame') return;
 
+        const dmPN = getDMHelper();
+
+        let _dmPromise = null;
+        const getDMPromise = () => {
+            return (_dmPromise || (_dmPromise = (new Promise(dmPN)).then(() => {
+                _dmPromise = null;
+            })))
+        };
+
         const cnt = insp(chat);
         const cProto = cnt.constructor.prototype || 0;
 
-        if (typeof cProto.urlChanged === 'function' && !cProto.urlChanged66 && !cProto.urlChangedAsync12) {
+
+        if (typeof cProto.urlChanged === 'function' && !cProto.urlChanged66) {
             cProto.urlChanged66 = cProto.urlChanged;
             let ath = 0;
             cProto.urlChangedAsync12 = async function () {
                 if (ath > 1e9) ath = 9;
                 const t = ++ath;
-                try {
-                    const chatframe = this.chatframe || this.$.chatframe;
-                    if (chatframe && chatframe.contentDocument === null) await Promise.resolve();
-                } catch (e) {
-
-                }
+                if (this.chatframe.contentDocument === null) await Promise.resolve();
+                if (t !== ath) return;
+                await getDMPromise();
                 if (t !== ath) return;
                 this.urlChanged66();
             }
