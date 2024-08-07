957a72aa23b6e1aefa3dc6426581882599a1207e
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-08-08 01:48:59 +0900

    YouTube Music: Audio Only | Fix TTP

--- youtube-music-audio-only.user.js
+++ youtube-music-audio-only.user.js
@@ -2,7 +2,7 @@
 // @name                YouTube Music: Audio Only
 // @description         No Video Streaming
 // @namespace           UserScript
-// @version             0.1.11
+// @version             0.1.12
 // @author              CY Fung
 // @match               https://music.youtube.com/*
 // @exclude             /^https?://\S+\.(txt|png|jpg|jpeg|gif|xml|svg|manifest|log|ini)[^\/]*$/
@@ -24,6 +24,79 @@
 (async function () {
     'use strict';
 
+
+    !window.TTP && (() => {
+        // credit to Benjamin Philipp
+        // original source: https://greasyfork.org/en/scripts/433051-trusted-types-helper
+
+        // --------------------------------------------------- Trusted Types Helper ---------------------------------------------------
+
+        const overwrite_default = false; // If a default policy already exists, it might be best not to overwrite it, but to try and set a custom policy and use it to manually generate trusted types. Try at your own risk
+        const prefix = `TTP`;
+        var passThroughFunc = function (string, sink) {
+            return string; // Anything passing through this function will be returned without change
+        }
+        var TTPName = "passthrough";
+        var TTP_default, TTP = { createHTML: passThroughFunc, createScript: passThroughFunc, createScriptURL: passThroughFunc }; // We can use TTP.createHTML for all our assignments even if we don't need or even have Trusted Types; this should make fallbacks and polyfills easy
+        var needsTrustedHTML = false;
+        function doit() {
+            try {
+                if (typeof window.isSecureContext !== 'undefined' && window.isSecureContext) {
+                    if (window.trustedTypes && window.trustedTypes.createPolicy) {
+                        needsTrustedHTML = true;
+                        if (trustedTypes.defaultPolicy) {
+                            log("TT Default Policy exists");
+                            if (overwrite_default)
+                                TTP = window.trustedTypes.createPolicy("default", TTP);
+                            else
+                                TTP = window.trustedTypes.createPolicy(TTPName, TTP); // Is the default policy permissive enough? If it already exists, best not to overwrite it
+                            TTP_default = trustedTypes.defaultPolicy;
+
+                            log("Created custom passthrough policy, in case the default policy is too restrictive: Use Policy '" + TTPName + "' in var 'TTP':", TTP);
+                        }
+                        else {
+                            TTP_default = TTP = window.trustedTypes.createPolicy("default", TTP);
+                        }
+                        log("Trusted-Type Policies: TTP:", TTP, "TTP_default:", TTP_default);
+                    }
+                }
+            } catch (e) {
+                log(e);
+            }
+        }
+
+        function log(...args) {
+            if ("undefined" != typeof (prefix) && !!prefix)
+                args = [prefix + ":", ...args];
+            if ("undefined" != typeof (debugging) && !!debugging)
+                args = [...args, new Error().stack.replace(/^\s*(Error|Stack trace):?\n/gi, "").replace(/^([^\n]*\n)/, "\n")];
+            console.log(...args);
+        }
+
+        doit();
+
+        // --------------------------------------------------- Trusted Types Helper ---------------------------------------------------
+
+        window.TTP = TTP;
+
+    })();
+
+    function createHTML(s) {
+        if (typeof TTP !== 'undefined' && typeof TTP.createHTML === 'function') return TTP.createHTML(s);
+        return s;
+    }
+
+    let trustHTMLErr = null;
+    try {
+        document.createElement('div').innerHTML = createHTML('1');
+    } catch (e) {
+        trustHTMLErr = e;
+    }
+
+    if (trustHTMLErr) {
+        console.log(`trustHTMLErr`, trustHTMLErr);
+        trustHTMLErr(); // exit userscript
+    }
 
     /** @type {globalThis.PromiseConstructor} */
     const Promise = (async () => { })().constructor; // YouTube hacks Promise in WaterFox Classic and "Promise.resolve(0)" nevers resolve.
