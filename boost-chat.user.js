46bc675e5d02134341c67c14e9c87f07dd30b0a3
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-12-21 23:36:11 +0900

    YouTube Boost Chat | added "Trusted Types Helper"
    added "Trusted Types Helper"

--- boost-chat.user.js
+++ boost-chat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                YouTube Boost Chat
 // @namespace           UserScripts
-// @version             0.1.55
+// @version             0.1.56
 // @license             MIT
 // @match               https://*.youtube.com/live_chat*
 // @grant               none
@@ -75,6 +75,63 @@ SOFTWARE.
     return;
   }
 
+
+  !window.TTP && (()=>{
+    // credit to Benjamin Philipp
+    // original source: https://greasyfork.org/en/scripts/433051-trusted-types-helper
+
+  // --------------------------------------------------- Trusted Types Helper ---------------------------------------------------
+
+  const overwrite_default = false; // If a default policy already exists, it might be best not to overwrite it, but to try and set a custom policy and use it to manually generate trusted types. Try at your own risk
+  const prefix = `TTP`;
+  var passThroughFunc = function(string, sink){
+  return string; // Anything passing through this function will be returned without change
+  }
+  var TTPName = "passthrough";
+  var TTP_default, TTP = {createHTML: passThroughFunc, createScript: passThroughFunc, createScriptURL: passThroughFunc}; // We can use TTP.createHTML for all our assignments even if we don't need or even have Trusted Types; this should make fallbacks and polyfills easy
+  var needsTrustedHTML = false;
+  function doit(){
+  try{
+    if(typeof window.isSecureContext !== 'undefined' && window.isSecureContext){
+      if (window.trustedTypes && window.trustedTypes.createPolicy){
+        needsTrustedHTML = true;
+        if(trustedTypes.defaultPolicy){
+          log("TT Default Policy exists");
+          if(overwrite_default)
+            TTP = window.trustedTypes.createPolicy("default", TTP);
+          else
+            TTP = window.trustedTypes.createPolicy(TTPName, TTP); // Is the default policy permissive enough? If it already exists, best not to overwrite it
+          TTP_default = trustedTypes.defaultPolicy;
+
+          log("Created custom passthrough policy, in case the default policy is too restrictive: Use Policy '" + TTPName + "' in var 'TTP':", TTP);
+        }
+        else{
+          TTP_default = TTP = window.trustedTypes.createPolicy("default", TTP);
+        }
+        log("Trusted-Type Policies: TTP:", TTP, "TTP_default:", TTP_default);
+      }
+    }
+  }catch(e){
+    log(e);
+  }
+  }
+
+  function log(...args){
+  if("undefined" != typeof(prefix) && !!prefix)
+    args = [prefix + ":", ...args];
+  if("undefined" != typeof(debugging) && !!debugging)
+    args = [...args, new Error().stack.replace(/^\s*(Error|Stack trace):?\n/gi, "").replace(/^([^\n]*\n)/, "\n")];
+  console.log(...args);
+  }
+
+  doit();
+
+  // --------------------------------------------------- Trusted Types Helper ---------------------------------------------------
+
+  window.TTP = TTP;
+
+  })();
+  
   const { appendChild: fragmentAppendChild } = ((h0) => h0)(new DocumentFragment());
 
   /* globals WeakRef:false */
