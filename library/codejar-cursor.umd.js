2f5a1de45efdc63f69224887a0b3a4c60148665f
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-03-16 07:30:15 +0900

    codejar

---
+++ library/codejar-cursor.umd.js
@@ -0,0 +1,85 @@
+/*
+MIT License
+
+Copyright (c) 2020 Anton Medvedev
+Copyright (c) 2025 CY Fung
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.CursorUtils = factory());
+}(this, (function () {
+    'use strict';
+
+    function cursorPosition(toStart = true) {
+        const s = window.getSelection();
+        if (s.rangeCount > 0) {
+            const cursor = document.createElement("span");
+            cursor.textContent = "|";
+            const r = s.getRangeAt(0).cloneRange();
+            r.collapse(toStart);
+            r.insertNode(cursor);
+            const { x, y, height } = cursor.getBoundingClientRect();
+            const top = (window.scrollY + y + height) + "px";
+            const left = (window.scrollX + x) + "px";
+            cursor.parentNode.removeChild(cursor);
+            return { top, left };
+        }
+        return undefined;
+    }
+
+    function selectedText() {
+        const s = window.getSelection();
+        if (s.rangeCount === 0)
+            return '';
+        return s.getRangeAt(0).toString();
+    }
+
+    function textBeforeCursor(editor) {
+        const s = window.getSelection();
+        if (s.rangeCount === 0)
+            return '';
+        const r0 = s.getRangeAt(0);
+        const r = document.createRange();
+        r.selectNodeContents(editor);
+        r.setEnd(r0.startContainer, r0.startOffset);
+        return r.toString();
+    }
+
+    function textAfterCursor(editor) {
+        const s = window.getSelection();
+        if (s.rangeCount === 0)
+            return '';
+        const r0 = s.getRangeAt(0);
+        const r = document.createRange();
+        r.selectNodeContents(editor);
+        r.setStart(r0.endContainer, r0.endOffset);
+        return r.toString();
+    }
+
+    return {
+        cursorPosition: cursorPosition,
+        selectedText: selectedText,
+        textBeforeCursor: textBeforeCursor,
+        textAfterCursor: textAfterCursor
+    };
+})));
