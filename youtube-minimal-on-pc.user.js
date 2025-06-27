f31946bc0dcaae70cd2772285af844c637ebb8e9
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-06-27 10:27:25 +0900

    Update youtube-minimal-on-pc.user.js

--- youtube-minimal-on-pc.user.js
+++ youtube-minimal-on-pc.user.js
@@ -1,3 +1,19 @@
+// ==UserScript==
+// @name         YouTube Minimal on PC
+// @version      0.6
+// @description  Watch YouTube with the least CPU usage
+// @namespace    http://tampermonkey.net/
+// @author       CY Fung
+// @license      MIT
+// @supportURL   https://github.com/cyfung1031/userscript-supports
+// @run-at       document-start
+// @match        https://www.youtube.com/*
+// @match        https://m.youtube.com/*
+// @icon         https://raw.githubusercontent.com/cyfung1031/userscript-supports/main/icons/youtube-minimal.png
+// @grant        GM_registerMenuCommand
+// @require      https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js
+// ==/UserScript==
+
 /*
 
 MIT License
@@ -23,21 +39,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
-// ==UserScript==
-// @name         YouTube Minimal on PC
-// @version      0.5
-// @description  Watch YouTube with the least CPU usage
-// @namespace    http://tampermonkey.net/
-// @author       CY Fung
-// @license      MIT
-// @supportURL   https://github.com/cyfung1031/userscript-supports
-// @run-at       document-start
-// @match        https://www.youtube.com/*
-// @match        https://m.youtube.com/*
-// @icon         https://raw.githubusercontent.com/cyfung1031/userscript-supports/main/icons/youtube-minimal.png
-// @grant        GM_registerMenuCommand
-// @require      https://cdnjs.cloudflare.com/ajax/libs/js-cookie/3.0.1/js.cookie.min.js#sha512=wT7uPE7tOP6w4o28u1DN775jYjHQApdBnib5Pho4RB0Pgd9y7eSkAV1BTqQydupYDB9GBhTcQQzyNMPMV3cAew==
-// ==/UserScript==
+
 "use strict";
 
 /* global Cookies */
